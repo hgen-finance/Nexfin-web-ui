7ce201ae3b973eb58e6180e10c4826ed48b672f8
pree-side <ppreeyash@gmail.com>
2021-11-08 08:11:03 +1100

    added icons to the wallet

--- src/components/HomeScreen/Gens.vue
+++ src/components/HomeScreen/Gens.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="container pt-20-S pt-40-XS">
-    <div class="w-100 fd-r jc-c ai-c fs-14-S fs-30-XS f-white-200 fw-600">
-      <img
-        src="@/assets/svg/gens.svg"
-        class="h-fix-20-S h-fix-20-S h-fix-35-XS mr-2-S mr-5-XS"
-      />
-      $GENS
-    </div>
-    <div class="w-100 fs-10-S fs-18-XS ta-c f-gray-500 pt-6-S pt-10-XS">
-      Stable Coin
+    <div class="w-100 fd-r jc-c ai-c">
+      <div class="w-20 fd-c ai-c">
+        <div class="w-100 fs-5-S fs-10-XS ta-c f-white-200 fw-600">
+          Stable Coin
+        </div>
+        <div
+          class="w-100-S w-100-XS fd-r fs-12-S fs-25-XS f-white-200 fw-600 jc-c ai-c"
+        >
+          <img
+            src="@/assets/svg/symbol-gens.png"
+            class="h-fix-20-S h-fix-20-S h-fix-35-XS mr-2-S mr-5-XS"
+          />
+          $GENS
+        </div>
+      </div>
+      <div class="w-20-S w-100-XS">
+        <AmButton
+          color="mcolor-100"
+          bColor="mcolor-100"
+          opacityEffect
+          scaleEffect
+          class="ml-2 ml-0-XS mt-0-S mt-10-XS fw-600"
+        >
+          <div class="w-100 tt-n  fs-6-S fs-18-XS fw-600">
+            Buy $GENS
+          </div>
+        </AmButton>
+      </div>
     </div>
     <div
       class="w-100 ta-c fs-8-S fs-16-XS f-white-200 px-0-S px-20-XS pt-6-S pt-15-XS"
