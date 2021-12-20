7d99b50439e558833518a8108e009d07eb099442
AI-Pree <sexypistolcs@gmail.com>
2021-12-20 14:41:46 +1100

    change url

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,10 +83,10 @@ export default {
 
   axios: {
     // TODO: change http to https
-    // baseUrl:
-    //   process.env.BASE_URL || "https://nexfin-backend-73znx.ondigitalocean.app/"
+    baseUrl:
+      process.env.BASE_URL || "https://nexfin-backend-73znx.ondigitalocean.app/"
     //baseUrl: process.env.baseUrl || "https://8882-118-208-107-33.ngrok.io/"
-    baseUrl: process.env.baseUrl || "http://192.168.43.112:3000/"
+    //baseUrl: process.env.baseUrl || "http://192.168.43.112:3000/"
     //baseUrl: process.env.baseUrl || "https://liquity-back.ambersoft.llc/"
   },
 
@@ -94,10 +94,10 @@ export default {
   env: {
     // TODO: change http to https
     // baseUrl:
-    //   process.env.BASE_URL ||
-    //   "https://nexfin-backend-73znx.ondigitalocean.app/",
+      process.env.BASE_URL ||
+      "https://nexfin-backend-73znx.ondigitalocean.app/",
     // baseUrl: process.env.BASE_URL || "https://8882-118-208-107-33.ngrok.io/",
-    baseUrl: process.env.BASE_URL || "http://192.168.43.112:3000/",
+    //baseUrl: process.env.BASE_URL || "http://192.168.43.112:3000/",
     //baseUrl: process.env.BASE_URL || "https://liquity-back.ambersoft.llc/",
     mint: "2U3Mf4umT4CpLhhdwpfmGiktyvhdrLrNNv4z4GgsXNMe"
   },
