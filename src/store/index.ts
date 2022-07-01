b3c094289f0b05a91d419a370b7bcde0f9f34c54
pree-side <ppreeyash@gmail.com>
2022-07-02 02:59:43 +1000

    change to negative 3

--- src/store/index.ts
+++ src/store/index.ts
@@ -192,7 +192,7 @@ export const actions = actionTree(
           // tslint:disable-next-line:no-console
           let pyth;
           const PERCENT = 100;
-          const CALL_PERCENT = -30;
+          const CALL_PERCENT = -3;
           const REFRESH_HOURS = 24;
 
           let prev_price = 32.3;
